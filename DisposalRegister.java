60ba489602e9bda564e5fe5a74dc646d86ae8fbc
Jo-jaehyeon <73457969+Jo-jaehyeon@users.noreply.github.com>
2020-10-29 22:44:11 +0900

    Update DisposalRegister.java

--- DisposalRegister.java
+++ DisposalRegister.java
@@ -57,65 +57,68 @@ public class DisposalRegister {
 				 System.out.println("\n유통기한 지난 상품들이 없습니다.");
 			 else
 			 {
-				 System.out.println("\n\n\n");
-				 System.out.println("폐기할 상품의 상품코드를 입력해주세요");	
+				 System.out.println("\n\n폐기할 상품의 상품코드를 입력해주세요");	
 			 }
 			 System.out.println("종료하시려면 \"완료\"를 입력하세요");	
-			 answer = scan.nextLine();
-			 
-			 if(!answer.equals("완료"))		//완료가 아니면
-			 {
-				 do
-				 {
-					 while( !checkBlank(answer) || answer.matches(".*[^a-z A-Z]+.*") || answer.length() != 6 )	//폐기 상품코드 예외처리
-					 {
-						 System.out.print("잘못된 입력, 다시 입력해주세요(ONLY 영어, 6글자, 공백제외): ");
-						 answer = scan.nextLine();
-						 if(answer.equals("완료"))		
-							 break;
-					 }
-					 if(!answer.equals("완료"))		//완료가 아니면
-					 {
-							for(int i=0; i < Category.size() ; i++)					//큰 카테고리 개수 만큼의 상품 파일을 읽기
+			 	
+			do
+			{
+				answer = scan.nextLine();
+				if(!answer.equals("완료"))		//완료가 아니면
+				{
+					while( !checkBlank(answer) || answer.matches(".*[^a-z A-Z]+.*") || answer.length() != 6 )	//폐기 상품코드 예외처리
+					{
+						System.out.print("잘못된 입력, 다시 입력해주세요(\"완료\" 또는 ONLY 영어, 6글자, 공백제외): ");
+						answer = scan.nextLine();
+						if(answer.equals("완료"))		
+							break;
+					}
+					if(!answer.equals("완료"))		//완료가 아니면
+					{
+						for(int i=0; i < Category.size() ; i++)					//큰 카테고리 개수 만큼의 상품 파일을 읽기
+						{
+							filename = String.valueOf((char) ('A' + i/26) + String.valueOf((char) ('A' + i%26))) + ".txt";
+							products = fileio.readProduct(filename);
+									
+							dis = 3;	//어떠한 상품과 코드가 맞지 않는 상태
+							for(int j = 0; j<products.size(); j++)
 							{
-								filename = String.valueOf((char) ('A' + i/26) + String.valueOf((char) ('A' + i%26))) + ".txt";
-								products = fileio.readProduct(filename);
-								
-								dis = 3;	//어떠한 상품과 코드가 맞지 않는 상태
-								for(int j = 0; j<products.size(); j++)
+								if(Integer.parseInt(products.get(j).getEpdate()) < Integer.parseInt(today))		//유통기한 < 오늘 날짜(폐기 대상 상품)
 								{
-									if(Integer.parseInt(products.get(j).getEpdate()) < Integer.parseInt(today))		//유통기한 < 오늘 날짜(폐기 대상 상품)
+									if(products.get(j).getCode().equals(answer.toUpperCase()))	//입력한 상품과 같다면 대소문자가 달라도 문자열이 같다면
 									{
-										if(products.get(j).getCode().equals(answer))	//입력한 상품과 같다면
-										{
-											dis = 1;
-											break;
-										}																				
-									}
-									else if(products.get(j).getCode().equals(answer))	//폐기 대상은 아닌 상품과 같다면
-									{
-										dis = 2;
+										dis = 1;
 										break;
-									}	
+									}																				
 								}
-								
+								else if(products.get(j).getCode().equals(answer))	//폐기 대상은 아닌 상품과 같다면
+								{
+									dis = 2;
+									break;
+								}	
 							}
-							
-						 if(dis == 1)
-						 {
-							 answer = answer.toUpperCase();
-							 CheckDisposal(answer);
-						 }
-						 else if(dis == 2)
-							 System.out.println("폐기 대상 상품이 아닙니다.");
-						 else
-							 System.out.println("존재하지 않는 상품입니다.");
-					 }
-				 }while(dis != 1);
-			 }
+									
+						}							
+						if(dis == 1)
+						{
+							answer = answer.toUpperCase();
+							CheckDisposal(answer);
+						}
+						else if(dis == 2)
+							System.out.println("폐기 대상 상품이 아닙니다. 다시 입력해주세요.");
+						else
+							System.out.println("존재하지 않는 상품입니다. 다시 입력해주세요.");
+					}
+					else	//완료면
+						break;
+				}
+				else		//완료입력
+					break;
+			}while(dis != 1);
 		}while(!answer.equals("완료"));
-	
 	}
+	
+
 	
 	public void CheckDisposal(String procode)
 	{
@@ -123,13 +126,13 @@ public class DisposalRegister {
 		 ArrayList<String> contents = new ArrayList();
 		 String filename, yn;
 			
-		 System.out.print("해당 상품(" + procode + ")을 정말 폐기하시겠습니까?(Y/N) ");
+		 System.out.print("해당 상품(" + procode + ")을 정말 폐기하시겠습니까?(Y/N): ");
 		 yn = scan.nextLine();
 		 
 		 while(!( yn.equals("Y") || yn.equals("N") ) || !checkBlank(yn))
 		 {
 			 System.out.print("잘못된 입력, 다시 입력해주세요(ONLY Y/N 1글자, 공백제외): ");
-			 procode = scan.nextLine();
+			 yn = scan.nextLine();
 		 }
 		 
 		 if(yn.equals("N"))
