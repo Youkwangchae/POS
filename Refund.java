2252b82042bacf680b6c3529df4a3fc01a112590
eric1306 <64396933+eric1306@users.noreply.github.com>
2020-10-29 20:23:50 +0900

    최종의최종본-강건우

--- Refund.java
+++ Refund.java
@@ -19,6 +19,7 @@ public class Refund {
 	private CashManager cm;
 	private ScreenClear sc;
 	private String isCashCharge; // 현금충전 여부 체크하기 위한 변수
+	private boolean isOtherPC;
 
 	public Refund(Db date) {
 		this.scan = new Scanner(System.in);
@@ -30,141 +31,152 @@ public class Refund {
 	}
 
 	public void RefundS() { // 환불 진행하는 함수
-
-		do { // 문법 형식에 맞는 문자열을 입력받을구입 날짜 때 까지 입력받는 do-while문
-			System.out.print("결제 코드를 입력하세요: ");
-			PayCode = scan.nextLine();
-		} while (!(checkL_PayCode(PayCode) & checkI_PayCode(PayCode) & checkBlank(PayCode) & checkR_PayCode(PayCode)));
-
-		String r_possible_Pd = PayCode.substring(0, 6);// 환불 가능한 날짜인지 판단을 위한 추출-결제 날짜
-		int year1 = Integer.parseInt(r_possible_Pd.substring(0, 2));// 구매날짜 년 뒤에 두자리
-		int month1 = Integer.parseInt(r_possible_Pd.substring(2, 4)); // 구매날짜 월
-		int day1 = Integer.parseInt(r_possible_Pd.substring(4, 6)); // 구매날짜 일
-
-		String today = date.getLast_date();// 현재 날짜 가지고 오기
-		today_year = Integer.parseInt(today.substring(2, 4));// 현재날짜 년 뒤에 두자리
-		today_month = Integer.parseInt(today.substring(4, 6));// 현재날짜 월
-		today_day = Integer.parseInt(today.substring(6, 8));// 현재날짜 일
-
-		int day_term1 = checkD(year1, month1, day1);// 구매날짜와 현재 날짜 차이 계산
-		if (day_term1 >= 7) {// 여기서 현재 날짜와 구매날짜 비교 7일 넘을시 환불 불가 판정후 메인메뉴로 복귀
-			System.out.println("구입 후 일주일이 지났기 때문에 환불이 불가능합니다.\n메인메뉴로 돌아갑니다.");
-			return; // 종료분기
-		}
-		int count = 0;// while빠져나오게 하기 위해 만든 변수
+		isOtherPC=false;
 		while (true) {
-			list = Product_list.get(PayCode); // Paycode를 key값으로 하는 value를 받는 ArrayList<Product>
-			System.out.println("          결제 리스트            ");
-			System.out.println("-------------------------");
-			for (Product i : list) {
-				System.out.print(i);
+			do { // 문법 형식에 맞는 문자열을 입력받을구입 날짜 때 까지 입력받는 do-while문
+				System.out.print("결제 코드를 입력하세요(종료하고싶다면 완료를 입력하시오): ");
+				PayCode = scan.nextLine();
+			} while (!(checkL_PayCode(PayCode) & checkI_PayCode(PayCode) & checkBlank(PayCode)
+					& checkR_PayCode(PayCode)));
+			if (PayCode.equals("완료")) {
+				System.out.println("환불을 종료하고 메인메뉴로 이동합니다.");
+				return;
 			}
-			System.out.println("      환불 가능 상품 리스트       ");
-			System.out.println("-------------------------");
+			String r_possible_Pd = PayCode.substring(0, 6);// 환불 가능한 날짜인지 판단을 위한 추출-결제 날짜
+			int year1 = Integer.parseInt(r_possible_Pd.substring(0, 2));// 구매날짜 년 뒤에 두자리
+			int month1 = Integer.parseInt(r_possible_Pd.substring(2, 4)); // 구매날짜 월
+			int day1 = Integer.parseInt(r_possible_Pd.substring(4, 6)); // 구매날짜 일
 
-			for (Product i : list) { // list의 모든 상품들을 출력하는 for문
-				String ep = i.getEpdate(); // 해당 상품의 유통기한을 점검하기 위해서 Product의 유통기한 값을 가져와서 현재 시간과 비교한다.
-				int day_term2 = ep_date(ep);// 유통기한 체크
-				if (day_term2 < 0) {// 만약 날짜 차이가 30일 미만 즉, 유통기한이 아직 지나지 않았다면
-					System.out.print(i);// 해당상품정보를 출력
-				}
+			String today = date.getLast_date();// 현재 날짜 가지고 오기
+			today_year = Integer.parseInt(today.substring(2, 4));// 현재날짜 년 뒤에 두자리
+			today_month = Integer.parseInt(today.substring(4, 6));// 현재날짜 월
+			today_day = Integer.parseInt(today.substring(6, 8));// 현재날짜 일
+
+			int day_term1 = checkD(year1, month1, day1);// 구매날짜와 현재 날짜 차이 계산
+			if (day_term1 >= 7) {// 여기서 현재 날짜와 구매날짜 비교 7일 넘을시 환불 불가 판정후 메인메뉴로 복귀
+				System.out.println("구입 후 일주일이 지났기 때문에 환불이 불가능합니다.\n메인메뉴로 돌아갑니다.");
+				return; // 종료분기
 			}
-			// 상품 리스트 출력 구문 넣기
-			System.out.print("환불한 상품 코드를 입력해주세요(환불을 멈추고싶다면 완료를  입력해주세요): ");
-			product_code = scan.nextLine();
-			product_code = product_code.toUpperCase();
-			if ((checkL_Product_code(product_code)) & (checkC_Product_code(product_code) & checkBlank(product_code))) {// 상품코드
-																														// //
-																														// 문법규칙																					// 																										// 검사
+			while (true) {
+				list = Product_list.get(PayCode); // Paycode를 key값으로 하는 value를 받는 ArrayList<Product>
+				System.out.println("          결제 리스트            ");
+				System.out.println("-------------------------");
 				for (Product i : list) {
-					String ep = i.getEpdate();
-					int day_term = ep_date(ep);
-					if ((i.getCode().equals(product_code)) & (day_term < 0)) {// 상품코드와 일치하는 상품의 인덱스 번호 찾기
-						index = list.indexOf(i);// 일치한다면, 해당 상품의 인덱스를 위에서 선언했던 index에 호출한다
-						break;
-					} else
-						index = -1;
+					System.out.print(i);
 				}
-				if (index == -1) { // indexOf는 값을 찾지 못했다면 -1을 리턴하기 때문에 예외처리
-					System.out.println("환불 가능리스트에 존재하지 않는 상품코드 입니다.");
-					continue;// 다음 while문으로 진행
-				} else {
-					while (true) {
-						System.out.print(list.get(index).getName() + "을(를) 환불하시겠습니까?(Y/N): ");
-						String YN = scan.nextLine(); // 환불여부 YN으로 재차 묻기
-						if ((checkL_YN(YN)) & (checkC_YN(YN)) & (checkBlank(YN))) {
-							if (YN.equals("N")) {
-								System.out.println("상품코드 입력으로 넘어갑니다.");
-								break;
-							} else {// 해당 // 결제목록에서 // 결제한 // 상품 삭제
-								int P_price = list.get(index).getPrice();// 상품 가격 가져오기
-								if (list.get(index).getIsPayByCash()) {// 현금 결제인 경우
-									System.out.println(P_price + "원의 현금 환불을 진행합니다.\n......\n");
-									int count2 = 0;
-									for (int i = 7; i >= 0; i--) {
-										int mok = P_price / cm.getKeyindex(i);
-										if (mok == 0)
-											continue;
-										else {
-											P_price -= cm.getKeyindex(i) * mok;
-											if (date.getCash().get(cm.getKeyindex(i)) < mok) {
-												while (true) {
-													System.out.print("잔돈이 부족합니다." + cm.getKeyindex(i) + "원이 "
-															+ (mok - date.getCash().get(cm.getKeyindex(i)))
-															+ "개 더 필요합니다. 현금 충전을 진행하시겠습니까? : ");
-													String a = scan.nextLine();
-													setIsCashCharge(a);
-													if (isCashCharge.equals("Y")) {
-														cm.ManageCash();
-														break;
-													} else if (isCashCharge.equals("N")) {
-														System.out.println("상품 코드 입력 부분으로 넘어갑니다.");
-														count2=1;
-														break;
-													} else {
-														System.out.println("올바르지 않은 입력입니다.");
-													}
-												}
-											} else {
-												setIsCashCharge("Y");
-												date.setCash(cm.getKeyindex(i), mok, true);
-											}
-										}if(count2!=0)
-											break;
-									}
-								} else { // 카드 결제인 경우
-									System.out.println(P_price + "원의 카드 환불을 진행합니다\n....\n");
-									isCashCharge="Y";
-									P_price=0;
-								}
-								if (isCashCharge.equals("Y") && YN.equals("Y") && (P_price == 0)) {//현금 환불기능
-									date.addProduct(list.get(index));
-									list.remove(index);
-									System.out.println(PayCode+product_code);
-									date.removePayment(PayCode, product_code);
-									sc=new ScreenClear();
-									System.out.println("환불이 완료됐습니다");
-								}
-								break;
-							}
-						} else {
-							System.out.println("환불여부질문으로 되돌아갑니다.");
-							continue;
-						}
+				System.out.println("      환불 가능 상품 리스트       ");
+				System.out.println("-------------------------");
+
+				for (Product i : list) { // list의 모든 상품들을 출력하는 for문
+					String ep = i.getEpdate(); // 해당 상품의 유통기한을 점검하기 위해서 Product의 유통기한 값을 가져와서 현재 시간과 비교한다.
+					int day_term2 = ep_date(ep);// 유통기한 체크
+					if (day_term2 < 0) {// 만약 날짜 차이가 30일 미만 즉, 유통기한이 아직 지나지 않았다면
+						System.out.print(i);// 해당상품정보를 출력
 					}
 				}
-			} else if (product_code.equals("완료")) {
-				System.out.println("완료를 입력했습니다. 메인메뉴로 돌아갑니다.");
-				return;// 종료 분기
-			} else {
+				// 상품 리스트 출력 구문 넣기
+				System.out.print("환불한 상품 코드를 입력해주세요(환불을 멈추고싶다면 완료를  입력해주세요): ");
+				product_code = scan.nextLine();
+				product_code = product_code.toUpperCase();
+				if ((checkL_Product_code(product_code))
+						& (checkC_Product_code(product_code) & checkBlank(product_code))) { // //
+																							// 검사
+					for (Product i : list) {
+						String ep = i.getEpdate();
+						int day_term = ep_date(ep);
+						if ((i.getCode().equals(product_code)) & (day_term < 0)) {// 상품코드와 일치하는 상품의 인덱스 번호 찾기
+							index = list.indexOf(i);// 일치한다면, 해당 상품의 인덱스를 위에서 선언했던 index에 호출한다
+							break;
+						} else
+							index = -1;
+					}
+					if (index == -1) { // indexOf는 값을 찾지 못했다면 -1을 리턴하기 때문에 예외처리
+						System.out.println("환불 가능리스트에 존재하지 않는 상품코드 입니다.");
+						continue;// 다음 while문으로 진행
+					} else {
+						while (true) {
+							System.out.print(list.get(index).getName() + "을(를) 환불하시겠습니까?(Y/N): ");
+							String YN = scan.nextLine(); // 환불여부 YN으로 재차 묻기
+							if ((checkL_YN(YN)) & (checkC_YN(YN)) & (checkBlank(YN))) {
+								if (YN.equals("N")) {
+									System.out.println("상품코드 입력으로 넘어갑니다.");
+									break;
+								} else if (YN.equals("Y")) {// 해당 // 결제목록에서 // 결제한 // 상품 삭제
+									int P_price = list.get(index).getPrice();// 상품 가격 가져오기
+									if (list.get(index).getIsPayByCash()) {// 현금 결제인 경우
+										System.out.println(P_price + "원의 현금 환불을 진행합니다.\n......\n");
+										int count2 = 0;
+										for (int i = 7; i >= 0; i--) {
+											int mok = P_price / cm.getKeyindex(i);
+											if (mok == 0)
+												continue;
+											else {
+												P_price -= cm.getKeyindex(i) * mok;
+												if (date.getCash().get(cm.getKeyindex(i)) < mok) {
+													while (true) {
+														System.out.print("잔돈이 부족합니다." + cm.getKeyindex(i) + "원이 "
+																+ (mok - date.getCash().get(cm.getKeyindex(i)))
+																+ "개 더 필요합니다. 현금 충전을 진행하시겠습니까? : ");
+														String a = scan.nextLine();
+														setIsCashCharge(a);
+														if (isCashCharge.equals("Y")) {
+															cm.ManageCash(true);
+															break;
+														} else if (isCashCharge.equals("N")) {
+															System.out.println("상품 코드 입력 부분으로 넘어갑니다.");
+															count2 = 1;
+															break;
+														} else {
+															System.out.println("올바르지 않은 입력입니다.");
+														}
+													}
+												} else {
+													setIsCashCharge("Y");
+													date.setCash(cm.getKeyindex(i), mok, true);
+												}
+											}
+											if (count2 != 0)
+												break;
+										}
+									} else { // 카드 결제인 경우
+										System.out.println(P_price + "원의 카드 환불을 진행합니다\n....\n");
+										isCashCharge = "Y";
+										P_price = 0;
+									}
+									if (isCashCharge.equals("Y") && YN.equals("Y") && (P_price == 0)) {// 현금 환불기능
+										date.addProduct(list.get(index));
+										list.remove(index);
+										date.removePayment(PayCode, product_code);
+										sc = new ScreenClear();
+										System.out.println("환불이 완료됐습니다");
+									}
+									break;
+								}
+							} else {
+								System.out.println("환불여부질문으로 되돌아갑니다.");
+								continue;
+							}
+						}
+					}
+				} else if (product_code.equals("완료")) {
+					System.out.println("완료를 입력했습니다. 메인메뉴로 돌아갑니다.");
+					return;// 종료 분기
+				} else {
+					continue;
+				}
+				if (Product_list.containsKey(PayCode)) { // 결제 코드가 있으면 참 없으면 거짓
+					isOtherPC=Product_list.containsKey(PayCode);// 여기서 저 함수 출력
+					continue;
+				} else
+					break;
+			}
+			if(isOtherPC) {
 				continue;
-			}
-			if (count != 0) {
-				count = 0;
+			}else
 				break;
-			}
 		}
 	}
+
 	public void setIsCashCharge(String isCashCharge) {
 		this.isCashCharge = isCashCharge;
 	}
@@ -187,6 +199,8 @@ public class Refund {
 	}
 
 	public boolean checkL_PayCode(String PayCode) {// 길이 체크하는 함수
+		if (PayCode.equals("완료"))
+			return true;
 		if (PayCode.length() == 9)
 			return true;
 		else if (PayCode.length() > 9) {
@@ -199,6 +213,8 @@ public class Refund {
 	}
 
 	public boolean checkI_PayCode(String PayCode) {// 숫자인지 체크하는 함수
+		if (PayCode.equals("완료"))
+			return true;
 		for (char c : PayCode.toCharArray()) {
 			if ((c >= 48) && (c <= 57)) {
 				continue;
@@ -211,8 +227,10 @@ public class Refund {
 	}
 
 	public boolean checkBlank(String PayCode) {// 선후 공백 체크
-		String B_PayCode=PayCode.replaceAll("\\s+", "");
-		if(B_PayCode.equals(PayCode)) {
+		if (PayCode.equals("완료"))
+			return true;
+		String B_PayCode = PayCode.replaceAll("\\s+", "");
+		if (B_PayCode.equals(PayCode)) {
 			return true;
 		}
 		System.out.println("잘못된 입력 입니다-공백이 들어있습니다");
@@ -220,6 +238,8 @@ public class Refund {
 	}
 
 	public boolean checkR_PayCode(String PayCode) { // 실제로 존재하는 결제코드인지 체크
+		if (PayCode.equals("완료"))
+			return true;
 		String NonBPC = new String(PayCode);
 		NonBPC = NonBPC.replaceAll("\\s+", "");// 실제 체크를 위해서 공백 체크
 		for (String key : Product_list.keySet()) {
